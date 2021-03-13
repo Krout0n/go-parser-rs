553e7fcbbd235243bc393e58dafed674ed6ec5fe
Krouton <me+git@tokinia.me>
2021-03-14 02:29:45 +0900

    Add parse_octal_digit.

--- src/letter_and_digit.rs
+++ src/letter_and_digit.rs
@@ -1,4 +1,9 @@
-use nom::{character::complete::digit1, IResult};
+use nom::{
+    character::complete::{digit1, one_of},
+    combinator::recognize,
+    multi::many1,
+    IResult,
+};
 
 ///
 /// ```
@@ -10,4 +15,16 @@ use nom::{character::complete::digit1, IResult};
 /// ```
 pub fn parse_decimal_digit(s: &str) -> IResult<&str, &str> {
     digit1(s)
+}
+
+///
+/// ```
+/// use go_parser_rs::letter_and_digit::parse_octal_digit;
+/// use nom::{Err, error::{Error,ErrorKind}};
+/// assert_eq!(parse_octal_digit("21c"), Ok(("c", "21")));
+/// assert_eq!(parse_octal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
+/// assert_eq!(parse_octal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
+/// ```
+pub fn parse_octal_digit(s: &str) -> IResult<&str, &str> {
+    recognize(many1(one_of("0123456")))(s)
 }
