22522041730eb270cc7da6e9558771e0c7e39687
Krouton <me+git@tokinia.me>
2021-03-14 02:36:16 +0900

    Fix parse_decimal_digit / parse_octal_digit

--- src/letter_and_digit.rs
+++ src/letter_and_digit.rs
@@ -1,30 +1,25 @@
-use nom::{
-    character::complete::{digit1, one_of},
-    combinator::recognize,
-    multi::many1,
-    IResult,
-};
+use nom::{character::complete::one_of, IResult};
 
 ///
 /// ```
 /// use go_parser_rs::letter_and_digit::parse_decimal_digit;
 /// use nom::{Err, error::{Error,ErrorKind}};
-/// assert_eq!(parse_decimal_digit("21c"), Ok(("c", "21")));
-/// assert_eq!(parse_decimal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::Digit))));
-/// assert_eq!(parse_decimal_digit(""), Err(Err::Error(Error::new("", ErrorKind::Digit))));
+/// assert_eq!(parse_decimal_digit("21c"), Ok(("1c", '2')));
+/// assert_eq!(parse_decimal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
+/// assert_eq!(parse_decimal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
 /// ```
-pub fn parse_decimal_digit(s: &str) -> IResult<&str, &str> {
-    digit1(s)
+pub fn parse_decimal_digit(s: &str) -> IResult<&str, char> {
+    one_of("0123456789")(s)
 }
 
 ///
 /// ```
 /// use go_parser_rs::letter_and_digit::parse_octal_digit;
 /// use nom::{Err, error::{Error,ErrorKind}};
-/// assert_eq!(parse_octal_digit("21c"), Ok(("c", "21")));
+/// assert_eq!(parse_octal_digit("21c"), Ok(("1c", '2')));
 /// assert_eq!(parse_octal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
 /// assert_eq!(parse_octal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
 /// ```
-pub fn parse_octal_digit(s: &str) -> IResult<&str, &str> {
-    recognize(many1(one_of("0123456")))(s)
+pub fn parse_octal_digit(s: &str) -> IResult<&str, char> {
+    one_of("01234567")(s)
 }
