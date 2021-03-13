4cdf7e4bf82c369a8f87cf7800bd66d07cadf754
Krouton <me+git@tokinia.me>
2021-03-13 17:26:20 +0900

    Wrap identifier / symbol / identifier.

---
+++ src/parse_util.rs
@@ -0,0 +1,30 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{alphanumeric1, char, space0},
+    IResult,
+};
+
+pub fn reserved(keyword: &'static str) -> impl Fn(&str) -> IResult<&str, ()> {
+    move |s: &str| {
+        let (s, _) = space0(s)?;
+        let (s, _) = tag(keyword)(s)?;
+        let (s, _) = space0(s)?;
+        Ok((s, ()))
+    }
+}
+
+pub fn symbol(sym: char) -> impl Fn(&str) -> IResult<&str, ()> {
+    move |s: &str| {
+        let (s, _) = space0(s)?;
+        let (s, _) = char(sym)(s)?;
+        let (s, _) = space0(s)?;
+        Ok((s, ()))
+    }
+}
+
+pub fn identifier(s: &str) -> IResult<&str, &str> {
+    let (s, _) = space0(s)?;
+    let (s, ident) = alphanumeric1(s)?;
+    let (s, _) = space0(s)?;
+    Ok((s, ident))
+}
