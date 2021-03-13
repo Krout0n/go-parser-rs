232d6366a540860904a5c23b091e7a95cde96164
Krouton <me+git@tokinia.me>
2021-03-13 23:08:10 +0900

    Fix parse_util.

--- src/parse_util.rs
+++ src/parse_util.rs
@@ -1,13 +1,14 @@
 use nom::{
     bytes::complete::tag,
     character::complete::{alphanumeric1, char, space0},
+    combinator::not,
     IResult,
 };
 
 pub fn reserved(keyword: &'static str) -> impl Fn(&str) -> IResult<&str, ()> {
     move |s: &str| {
-        let (s, _) = space0(s)?;
         let (s, _) = tag(keyword)(s)?;
+        let (s, _) = not(alphanumeric1)(s)?;
         let (s, _) = space0(s)?;
         Ok((s, ()))
     }
@@ -15,7 +16,6 @@ pub fn reserved(keyword: &'static str) -> impl Fn(&str) -> IResult<&str, ()> {
 
 pub fn symbol(sym: char) -> impl Fn(&str) -> IResult<&str, ()> {
     move |s: &str| {
-        let (s, _) = space0(s)?;
         let (s, _) = char(sym)(s)?;
         let (s, _) = space0(s)?;
         Ok((s, ()))
@@ -23,7 +23,6 @@ pub fn symbol(sym: char) -> impl Fn(&str) -> IResult<&str, ()> {
 }
 
 pub fn identifier(s: &str) -> IResult<&str, &str> {
-    let (s, _) = space0(s)?;
     let (s, ident) = alphanumeric1(s)?;
     let (s, _) = space0(s)?;
     Ok((s, ident))
