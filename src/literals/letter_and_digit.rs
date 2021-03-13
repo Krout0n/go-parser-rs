a34f540ebdb8ad8cdcaaa60d40c3344fcb59c2c7
Krouton <me+git@tokinia.me>
2021-03-14 02:45:01 +0900

    Remove parse.

--- src/literals/letter_and_digit.rs
+++ src/literals/letter_and_digit.rs
@@ -2,48 +2,48 @@ use nom::{character::complete::one_of, IResult};
 
 ///
 /// ```
-/// use go_parser_rs::literals::letter_and_digit::parse_decimal_digit;
+/// use go_parser_rs::literals::letter_and_digit::decimal_digit;
 /// use nom::{Err, error::{Error,ErrorKind}};
-/// assert_eq!(parse_decimal_digit("21c"), Ok(("1c", '2')));
-/// assert_eq!(parse_decimal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
-/// assert_eq!(parse_decimal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
+/// assert_eq!(decimal_digit("21c"), Ok(("1c", '2')));
+/// assert_eq!(decimal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
+/// assert_eq!(decimal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
 /// ```
-pub fn parse_decimal_digit(s: &str) -> IResult<&str, char> {
+pub fn decimal_digit(s: &str) -> IResult<&str, char> {
     one_of("0123456789")(s)
 }
 
 ///
 /// ```
-/// use go_parser_rs::literals::letter_and_digit::parse_octal_digit;
+/// use go_parser_rs::literals::letter_and_digit::octal_digit;
 /// use nom::{Err, error::{Error,ErrorKind}};
-/// assert_eq!(parse_octal_digit("21c"), Ok(("1c", '2')));
-/// assert_eq!(parse_octal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
-/// assert_eq!(parse_octal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
+/// assert_eq!(octal_digit("21c"), Ok(("1c", '2')));
+/// assert_eq!(octal_digit("c1"), Err(Err::Error(Error::new("c1", ErrorKind::OneOf))));
+/// assert_eq!(octal_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
 /// ```
-pub fn parse_octal_digit(s: &str) -> IResult<&str, char> {
+pub fn octal_digit(s: &str) -> IResult<&str, char> {
     one_of("01234567")(s)
 }
 
 ///
 /// ```
-/// use go_parser_rs::literals::letter_and_digit::parse_binary_digit;
+/// use go_parser_rs::literals::letter_and_digit::binary_digit;
 /// use nom::{Err, error::{Error,ErrorKind}};
-/// assert_eq!(parse_binary_digit("01c"), Ok(("1c", '0')));
-/// assert_eq!(parse_binary_digit("1c"), Ok(("c", '1')));
-/// assert_eq!(parse_binary_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
+/// assert_eq!(binary_digit("01c"), Ok(("1c", '0')));
+/// assert_eq!(binary_digit("1c"), Ok(("c", '1')));
+/// assert_eq!(binary_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
 /// ```
-pub fn parse_binary_digit(s: &str) -> IResult<&str, char> {
+pub fn binary_digit(s: &str) -> IResult<&str, char> {
     one_of("01")(s)
 }
 
 ///
 /// ```
-/// use go_parser_rs::literals::letter_and_digit::parse_hex_digit;
+/// use go_parser_rs::literals::letter_and_digit::hex_digit;
 /// use nom::{Err, error::{Error,ErrorKind}};
-/// assert_eq!(parse_hex_digit("21c"), Ok(("1c", '2')));
-/// assert_eq!(parse_hex_digit("c1"),Ok(("1", 'c')));
-/// assert_eq!(parse_hex_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
+/// assert_eq!(hex_digit("21c"), Ok(("1c", '2')));
+/// assert_eq!(hex_digit("c1"),Ok(("1", 'c')));
+/// assert_eq!(hex_digit(""), Err(Err::Error(Error::new("", ErrorKind::OneOf))));
 /// ```
-pub fn parse_hex_digit(s: &str) -> IResult<&str, char> {
+pub fn hex_digit(s: &str) -> IResult<&str, char> {
     one_of("0123456789abcdefABCDEF")(s)
 }
