7977ef32eb7d94859218441a0fbf0d5a5dcc53c8
Krouton <me+git@tokinia.me>
2021-03-14 13:06:32 +0900

    Add octal_digits / octal_lit.

--- src/literals/integer.rs
+++ src/literals/integer.rs
@@ -7,7 +7,42 @@ use nom::{
     IResult,
 };
 
-use super::letter_and_digit::hex_digit;
+use super::letter_and_digit::{hex_digit, octal_digit};
+
+/// octal_digits = octal_digit { [ "_" ] octal_digit }.
+/// ```
+/// use go_parser_rs::literals::integer::octal_digits;
+/// assert_eq!(octal_digits("17014114105727"), Ok(("", "17014114105727")));
+/// assert_eq!(octal_digits("170_1411_105727"), Ok(("", "170_1411_105727")));
+/// assert!(octal_digits("_42").is_err()); // an identifier, not an integer literal
+/// assert!(octal_digits("42_").is_err()); // invalid: _ must separate successive digits
+/// assert!(octal_digits("4__2").is_err()); // invalid: only one _ at a time
+/// ```
+pub fn octal_digits(s: &str) -> IResult<&str, &str> {
+    let (s, digits) = recognize(pair(octal_digit, many0(pair(opt(tag("_")), octal_digit))))(s)?;
+    let (s, _) = not(tag("_"))(s)?;
+    Ok((s, digits))
+}
+
+/// octal_lit = "0" [ "o" | "O" ] [ "_" ] octal_digits .
+///
+///```
+/// use go_parser_rs::literals::integer::octal_lit;
+/// assert_eq!(octal_lit("012347"), Ok(("", "012347")));
+/// assert_eq!(octal_lit("0o600"), Ok(("", "0o600")));
+/// assert_eq!(octal_lit("0O600"), Ok(("", "0O600")));
+/// assert_eq!(octal_lit("0o_67_7_2_40_6"), Ok(("", "0o_67_7_2_40_6")));
+/// assert!(octal_lit("0_O123457").is_err()); // invalid: _ must separate successive digits
+///```
+pub fn octal_lit(s: &str) -> IResult<&str, &str> {
+    recognize(tuple((
+        tag("0"),
+        opt(alt((tag("o"), tag("O")))),
+        opt(tag("_")),
+        octal_digits,
+    )))(s)
+}
+
 /// hex_digits = hex_digit { [ "_" ] hex_digit } .
 ///
 /// ```
