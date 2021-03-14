0e8d870acd84b70a2c3daa3080aaf4ee0c515ed0
Krouton <me+git@tokinia.me>
2021-03-14 11:24:46 +0900

    Add hex_lit.

--- src/literals/integer.rs
+++ src/literals/integer.rs
@@ -1,8 +1,9 @@
 use nom::{
+    branch::alt,
     bytes::complete::tag,
     combinator::{not, opt, recognize},
     multi::many0,
-    sequence::pair,
+    sequence::{pair, tuple},
     IResult,
 };
 
@@ -21,4 +22,22 @@ pub fn hex_digits(s: &str) -> IResult<&str, &str> {
     let (s, digits) = recognize(pair(hex_digit, many0(pair(opt(tag("_")), hex_digit))))(s)?;
     let (s, _) = not(tag("_"))(s)?;
     Ok((s, digits))
+}
+
+/// hex_lit = "0" ( "x" | "X" ) [ "_" ] hex_digits .
+///
+///```
+/// use go_parser_rs::literals::integer::hex_lit;
+/// assert_eq!(hex_lit("0xBadFace"), Ok(("", "0xBadFace")));
+/// assert_eq!(hex_lit("0xBad_Face"), Ok(("", "0xBad_Face")));
+/// assert_eq!(hex_lit("0x_67_7a_2f_cc_40_c6"), Ok(("", "0x_67_7a_2f_cc_40_c6")));
+/// assert!(hex_lit("0_xBadFace").is_err()); // invalid: _ must separate successive digits
+///```
+pub fn hex_lit(s: &str) -> IResult<&str, &str> {
+    recognize(tuple((
+        tag("0"),
+        alt((tag("x"), tag("X"))),
+        opt(tag("_")),
+        hex_digits,
+    )))(s)
 }
