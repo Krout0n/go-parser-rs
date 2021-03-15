c82902c40ec16de5c55f27e3f1c977627cb783ea
Krouton <me+git@tokinia.me>
2021-03-15 20:02:00 +0900

    Add rel_op.

--- src/expression/mod.rs
+++ src/expression/mod.rs
@@ -6,6 +6,34 @@ pub enum Expression {
     // UnaryExpr(UnaryExpr),
 }
 
+// Precedence    Operator
+//     5             *  /  %  <<  >>  &  &^
+//     4             +  -  |  ^
+//     3             ==  !=  <  <=  >  >=
+//     2             &&
+//     1             ||
+
+/// rel_op = "==" | "!=" | "<" | "<="| ">" | ">=" .
+/// ```
+/// use go_parser_rs::expression::rel_op;
+/// assert_eq!(rel_op("==1"), Ok(("1", "==")));
+/// assert_eq!(rel_op("!=1"), Ok(("1", "!=")));
+/// assert_eq!(rel_op("<3"), Ok(("3", "<")));
+/// assert_eq!(rel_op("<=a"), Ok(("a", "<=")));
+/// assert_eq!(rel_op(">a"), Ok(("a", ">")));
+/// assert_eq!(rel_op(">= a"), Ok(("a", ">=")));
+/// ```
+pub fn rel_op(s: &str) -> IResult<&str, &str> {
+    alt((
+        symbol("=="),
+        symbol("!="),
+        symbol("<="),
+        symbol(">="),
+        symbol("<"),
+        symbol(">"),
+    ))(s)
+}
+
 /// add_op = "+" | "-" | "|" | "^" .
 /// ```
 /// use go_parser_rs::expression::add_op;
@@ -17,28 +45,6 @@ pub enum Expression {
 /// ```
 pub fn add_op(s: &str) -> IResult<&str, &str> {
     alt((symbol("+"), symbol("-"), symbol("|"), symbol("^")))(s)
-}
-
-/// unary_op   = "+" | "-" | "!" | "^" | "*" | "&" | "<-" .
-/// ```
-/// use go_parser_rs::expression::unary_op;
-/// assert_eq!(unary_op("+1"), Ok(("1", "+")));
-/// assert_eq!(unary_op("-1"), Ok(("1", "-")));
-/// assert_eq!(unary_op("!true"), Ok(("true", "!")));
-/// assert_eq!(unary_op("^a"), Ok(("a", "^")));
-/// assert_eq!(unary_op("&1"), Ok(("1", "&")));
-/// assert_eq!(unary_op("<- ch"), Ok(("ch", "<-")));
-/// ```
-pub fn unary_op(s: &str) -> IResult<&str, &str> {
-    alt((
-        symbol("+"),
-        symbol("-"),
-        symbol("!"),
-        symbol("^"),
-        symbol("*"),
-        symbol("&"),
-        symbol("<-"),
-    ))(s)
 }
 
 /// mul_op = "*" | "/" | "%" | "<<" | ">>" | "&" | "&^" .
@@ -61,5 +67,27 @@ pub fn mul_op(s: &str) -> IResult<&str, &str> {
         symbol(">>"),
         symbol("&^"),
         symbol("&"),
+    ))(s)
+}
+
+/// unary_op   = "+" | "-" | "!" | "^" | "*" | "&" | "<-" .
+/// ```
+/// use go_parser_rs::expression::unary_op;
+/// assert_eq!(unary_op("+1"), Ok(("1", "+")));
+/// assert_eq!(unary_op("-1"), Ok(("1", "-")));
+/// assert_eq!(unary_op("!true"), Ok(("true", "!")));
+/// assert_eq!(unary_op("^a"), Ok(("a", "^")));
+/// assert_eq!(unary_op("&1"), Ok(("1", "&")));
+/// assert_eq!(unary_op("<- ch"), Ok(("ch", "<-")));
+/// ```
+pub fn unary_op(s: &str) -> IResult<&str, &str> {
+    alt((
+        symbol("+"),
+        symbol("-"),
+        symbol("!"),
+        symbol("^"),
+        symbol("*"),
+        symbol("&"),
+        symbol("<-"),
     ))(s)
 }
