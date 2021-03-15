acb816c24e277b6fe1698289e4daec5241c073f2
Krouton <me+git@tokinia.me>
2021-03-15 21:00:05 +0900

    Add literal::Literal

--- src/literals/mod.rs
+++ src/literals/mod.rs
@@ -1,2 +1,23 @@
+use self::integer::IntLit;
+
 pub mod integer;
 pub mod letter_and_digit;
+
+///
+/// Literal     = BasicLit | CompositeLit | FunctionLit .
+/// OperandName = identifier | QualifiedIdent .
+pub enum Literal<'a> {
+    /// BasicLit    = int_lit | float_lit | imaginary_lit | rune_lit | string_lit .
+    IntLit(IntLit<'a>),
+    FloatLit,
+    ImaginaryLit,
+    RuneLit,
+    StringLit(&'a str),
+    // TODO: Composite, FunctionLit...
+}
+
+impl<'a> Literal<'a> {
+    pub fn int_lit(i: IntLit<'a>) -> Self {
+        Self::IntLit(i)
+    }
+}
