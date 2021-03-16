af7f9f7dbf713a920c379cfdb664ce0fcbc6160d
Krouton <me+git@tokinia.me>
2021-03-17 00:39:17 +0900

    Add OperandName::parse.

--- src/expression/operand.rs
+++ src/expression/operand.rs
@@ -1,4 +1,8 @@
-use crate::{identifier::QualifiedIdent, literals::Literal};
+use nom::{branch::alt, combinator::map, IResult};
+
+use crate::{
+    astable::ASTable, identifier::QualifiedIdent, literals::Literal, parse_util::identifier,
+};
 
 use super::Expression;
 
@@ -9,7 +13,24 @@ pub enum Operand<'a> {
     Parenthesized(Expression),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum OperandName<'a> {
     Identifier(&'a str),
     QualifiedIdent(QualifiedIdent<'a>),
+}
+
+impl<'a> ASTable<'a> for OperandName<'a> {
+    ///```
+    /// use go_parser_rs::expression::operand::OperandName;
+    /// use go_parser_rs::identifier::QualifiedIdent;
+    /// use go_parser_rs::astable::ASTable;
+    /// assert_eq!(OperandName::parse("x.y"), Ok(("", OperandName::QualifiedIdent(QualifiedIdent{package_name: "x", identifier: "y"}))));
+    /// assert_eq!(OperandName::parse("vmw"), Ok(("", OperandName::Identifier("vmw"))));
+    ///```
+    fn parse(s: &'a str) -> IResult<&'a str, Self> {
+        alt((
+            map(QualifiedIdent::parse, Self::QualifiedIdent),
+            map(identifier, Self::Identifier),
+        ))(s)
+    }
 }
