56ee2f2d5278e7347eeed9e740809a75fc897ed8
Krouton <me+git@tokinia.me>
2021-03-16 21:29:41 +0900

    Add parse_int_lit / parse.

--- src/literals/mod.rs
+++ src/literals/mod.rs
@@ -1,4 +1,8 @@
-use self::integer::IntLit;
+use nom::{combinator::map, IResult};
+
+use crate::astable::ASTable;
+
+use self::integer::{int_lit, IntLit};
 
 pub mod integer;
 pub mod letter_and_digit;
@@ -17,7 +21,13 @@ pub enum Literal<'a> {
 }
 
 impl<'a> Literal<'a> {
-    pub fn int_lit(i: IntLit<'a>) -> Self {
-        Self::IntLit(i)
+    fn parse_int_lit(s: &'a str) -> IResult<&'a str, Self> {
+        map(int_lit, Self::IntLit)(s)
+    }
+}
+
+impl<'a> ASTable<'a> for Literal<'a> {
+    fn parse(s: &'a str) -> IResult<&'a str, Self> {
+        Self::parse_int_lit(s)
     }
 }
